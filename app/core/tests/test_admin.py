ebc1c13b636f4e7dc8ad516171d74dfbd85758d2
Andy Parinas <andy@budgetpetproducts.com.au>
2023-08-22 22:04:49 +1000

    completed admin for users

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -33,3 +33,19 @@ class AdminSiteTest(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+
+    def test_edit_user_page(self):
+        """ Test edit user page """
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+
+    def test_create_user_page(self):
+        """ test user create page works """
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
